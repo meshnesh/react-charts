cd7e8c834f93dd88d4051e3997e2d2b3fdc46c6d
Antony Ng'ang'a <antony_mn@Antonys-MBP.mshome.net>
2017-10-20 17:23:24 +0300

    finished code for application

---
+++ src/components/Chart.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import {Bar, Line, Pie} from 'react-chartjs-2';
+
+export default class Chart extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            chartData: props.chartData
+        }
+    }
+
+
+    static defaultProps={
+        displayTitle: true,
+        displayLegend: true,
+        legendPosition: 'right',
+        location: 'City'
+    }
+
+    render() {
+        return (
+            <div className="chart">
+                <Bar
+                    data={this.state.chartData}
+                    options={{
+                        title: {
+                            display: this.props.displayTitle,
+                            text: 'largest cities in '+this.props.location,
+                            fontSize: 25
+                        },
+                        legend:{
+                            display: this.props.displayLegend,
+                            position: this.props.legendPosition
+                        }
+                    }}
+                />
+            </div>
+        )
+    }
+}
